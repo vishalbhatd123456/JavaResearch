87ea3ae658c91ba7970ae975b99cf20692c12a64
Vishal D Bhat <60818841+vishalbhatd123456@users.noreply.github.com>
2024-07-24 05:31:31 +0000

    added TaskManager first class

---
+++ TaskManager/src/main/java/com/example/TaskManagerApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class TaskManagerApplication
+{
+    public static void main(String[] args)
+    {
+        SprintApplication.run(TaskManagerApplication.class, args);
+    }
+}
